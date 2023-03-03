65e0e886b749980513d057b95cf584f1c61ddd8e
SL1907 <samuel.lennard@icloud.com>
2023-03-03 11:55:40 +0000

    Add error handling

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-app.use('/', express.static('sites/testing'));
+const routes = [
+    { route: '/', directory: 'testing' }
+]
+
+for (let { route, directory } of routes) {
+    app.use(route, express.static(__dirname + `/sites/${directory}`));
+}
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + "/sites/error/error.html");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server now running at http://localhost:${PORT}/.`);
