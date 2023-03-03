c7b548b0510d5f244ec125bdbdcd20a3ca719a57
SL1907 <samuel.lennard@icloud.com>
2023-03-03 10:57:42 +0000

    Fix typos

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "files/root/index.html");
+    res.sendFile(__dirname + "/files/root/index.html");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server now running at http://localost:${PORT}/.`);
+    console.log(`Server now running at http://localhost:${PORT}/.`);
 });
